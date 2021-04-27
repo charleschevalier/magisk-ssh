a5d730c6d8dded7054b8a19722cd9298701d119b
charles <chevalier.charles@gmail.com>
2021-04-27 12:10:36 +0200

    Some fixes to put executables in system path + Version bumped to 0.21

--- module_data/customize.sh
+++ module_data/customize.sh
@@ -5,65 +5,51 @@
 # We use custom unzipping
 SKIPUNZIP=1
 
-if [ -d "/system/xbin" ]; then
-  BIN_DIR="xbin"
-else
-  BIN_DIR="bin"
-fi
-
-if [ -d "/system/lib64" ]; then
-  LIB_DIR="lib64"
-else
-  LIB_DIR="lib"
-fi
-
 ui_print "*******************************"
 ui_print "      OpenSSH for Android      "
 ui_print "*******************************"
 
 local TMPDIR="$MODPATH/tmp"
-ui_print "[0/7] Preparing module directory"
+ui_print "[0/6] Preparing module directory"
 mkdir -p "$TMPDIR"
 mkdir -p "$MODPATH/bin"
 mkdir -p "$MODPATH/lib"
 
-ui_print "[1/7] Extracting architecture unspecific module files"
-unzip -o "$ZIPFILE" 'common/opensshd.init' -d "$TMPDIR" >&2
+ui_print "[1/6] Extracting architecture unspecific module files"
+unzip -o "$ZIPFILE" 'common/opensshd.init' -d "$TMPDIR"
+unzip -o "$ZIPFILE" 'common/magisk_ssh_library_wrapper' -d "$TMPDIR"
+unzip -o "$ZIPFILE" 'common/service.sh' -d "$TMPDIR"
+unzip -o "$ZIPFILE" 'post-fs-data.sh' -d "$TMPDIR"
 mv "$TMPDIR/common/opensshd.init" "$MODPATH"
+mv "$TMPDIR/common/service.sh" "$MODPATH"
+mv "$TMPDIR/common/magisk_ssh_library_wrapper" "$MODPATH/bin"
+mv "$TMPDIR/post-fs-data.sh" "$MODPATH"
 
-ui_print "[2/7] Extracting libraries and binaries for $ARCH"
-unzip -o "$ZIPFILE" "arch/$ARCH/*" -d "$TMPDIR" >&2
+ui_print "[2/6] Extracting libraries and binaries for $ARCH"
+unzip -o "$ZIPFILE" "arch/$ARCH/*" -d "$TMPDIR"
 mv "$TMPDIR/arch/$ARCH/lib"/* "$MODPATH/lib"
 mv "$TMPDIR/arch/$ARCH/bin"/* "$MODPATH/bin"
 
-ui_print "[3/7] Creating symlinks"
-rm -rf "$MODPATH/system/$BIN_DIR"
-rm -rf "$MODPATH/system/$LIB_DIR"
-mkdir -p "$MODPATH/system/$BIN_DIR"
-mkdir -p "$MODPATH/system/$LIB_DIR"
-
-for f in scp sftp sftp-server ssh ssh-keygen sshd rsync autossh; do
-  ln -s $MODPATH/bin/$f "$MODPATH/system/$BIN_DIR/$f"
-done
-ln -s $MODPATH/lib/libcrypto.so.1.1 "$MODPATH/system/$LIB_DIR/libcrypto.so"
-
-ui_print "[4/7] Creating SSH user directories"
+ui_print "[3/6] Creating SSH user directories"
 mkdir -p /data/ssh
 mkdir -p /data/ssh/root/.ssh
 mkdir -p /data/ssh/shell/.ssh
 
 if [ -f /data/ssh/sshd_config ]; then
-  ui_print "[5/7] Found sshd_config, will not copy a default one"
+  ui_print "[4/6] Found sshd_config, will not copy a default one"
 else
-  ui_print "[5/7] Extracting sshd_config"
-  unzip -o "$ZIPFILE" 'common/sshd_config' -d "$TMPDIR" >&2
+  ui_print "[4/6] Extracting sshd_config"
+  unzip -o "$ZIPFILE" 'common/sshd_config' -d "$TMPDIR"
   mv "$TMPDIR/common/sshd_config" '/data/ssh/'
 fi
 
-ui_print "[6/7] Setting permissions"
+ui_print "[5/6] Setting permissions"
 set_perm_recursive $MODPATH 0 0 0755 0644
-set_perm_recursive "$MODPATH/system/" 0 0 0755 0755
+set_perm_recursive "$MODPATH/bin/" 0 0 0755 0755
+set_perm_recursive "$MODPATH/bin/" 0 0 0755 0755
 set_perm "$MODPATH/opensshd.init" 0 0 0755
+set_perm "$MODPATH/post-fs-data.sh" 0 0 0755
+set_perm "$MODPATH/service.sh" 0 0 0755
 set_perm /data/ssh/sshd_config 0 0 0600
 chown shell:shell /data/ssh/shell
 chown shell:shell /data/ssh/shell/.ssh
@@ -71,5 +57,5 @@ chown root:root /data/ssh/root
 chown root:root /data/ssh/root/.ssh
 chmod 700 /data/ssh/{shell,root}/.ssh
 
-ui_print "[7/7] Cleaning up"
+ui_print "[6/6] Cleaning up"
 rm -rf "$TMPDIR"
---
+++ module_data/post-fs-data.sh
@@ -0,0 +1,33 @@
+#!/system/bin/sh
+
+MODPATH=${0%/*}
+
+if [ -d "/system/xbin" ]; then
+  BIN_DIR="xbin"
+else
+  BIN_DIR="bin"
+fi
+
+rm -rf "$MODPATH/system"
+mkdir -p "$MODPATH/system/$BIN_DIR"
+
+for f in scp sftp sftp-server ssh ssh-keygen sshd; do
+  ln -s $MODPATH/bin/magisk_ssh_library_wrapper "$MODPATH/system/$BIN_DIR/$f"
+done
+for f in rsync autossh; do
+  ln -s $MODPATH/bin/$f "$MODPATH/system/$BIN_DIR/$f"
+done
+ln -s $MODPATH/lib/libcrypto.so.1.1 "$MODPATH/lib/libcrypto.so"
+
+set_perm_recursive "$MODPATH/system/" 0 0 0755 0755
+
+# Update launcher
+/data/adb/magisk/busybox sed -i 's|bindir=|bindir='$MODPATH'/bin|g' $MODPATH/bin/magisk_ssh_library_wrapper
+/data/adb/magisk/busybox sed -i 's|libdir=|libdir='$MODPATH'/lib|g' $MODPATH/bin/magisk_ssh_library_wrapper
+
+# Update opensshd.init
+/data/adb/magisk/busybox sed -i 's|prefix="$MODDIR/system"|prefix="/system"|g' $MODPATH/opensshd.init
+/data/adb/magisk/busybox sed -i 's|SSHD=$prefix/bin/sshd|SSHD=$prefix/'$BIN_DIR'/sshd|g' $MODPATH/opensshd.init
+/data/adb/magisk/busybox sed -i 's|SSH_KEYGEN=$prefix/bin/ssh-keygen|SSH_KEYGEN=$prefix/'$BIN_DIR'/ssh-keygen|g' $MODPATH/opensshd.init
+
+rm -f "$MODPATH/post-fs-data.sh"
